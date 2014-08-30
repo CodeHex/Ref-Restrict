37ce6b5af112cdd4db3eaf7da53f23612879272c
Ben Ibinson <codehex.net@gmail.com>
2014-08-30 14:19:39 +0100

    Created RefRestrict console app and auto copy output to solution folder

---
+++ src/RefRestrict/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefRestrict
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            Console.WriteLine("ERROR: The reference RR.Core is not valid in project RR.Data");
+            return 67;
+        }
+    }
+}
