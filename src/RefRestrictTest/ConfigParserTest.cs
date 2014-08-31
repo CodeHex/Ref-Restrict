605acf55a42b2e22c19a6b964561c8a251afacc0
Ben Ibinson <codehex.net@gmail.com>
2014-08-31 19:07:15 +0100

    Added Unit Testing Project

---
+++ src/RefRestrictTest/ConfigParserTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RefRestrict;
+
+namespace RefRestrictTest
+{
+    [TestClass]
+    public class ConfigParserTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(DirectoryNotFoundException))]
+        public void GetRuleSetForAConfigFileWhereDirectoryDoesntExist()
+        {
+            // Arrange
+            var badConfigFile = @"C:\IDoNotExist\InvalidConfig.xml";
+            var project = "RefRestrict";
+
+            // Act
+            ConfigParser.GetRuleSetForProject(badConfigFile, project);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FileNotFoundException))]
+        public void GetRuleSetForAConfigFileWhereFileDoesntExist()
+        {
+            // Arrange
+            var badConfigFile = @"C:\InvalidConfig.xml";
+            var project = "RefRestrict";
+
+            // Act
+            ConfigParser.GetRuleSetForProject(badConfigFile, project);
+        }
+    }
+}
