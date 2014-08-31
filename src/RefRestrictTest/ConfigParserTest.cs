629e7192f548929256dd62ad08eea2820a6e0adf
Ben Ibinson <codehex.net@gmail.com>
2014-08-31 19:37:47 +0100

    Added more unit tests

--- src/RefRestrictTest/ConfigParserTest.cs
+++ src/RefRestrictTest/ConfigParserTest.cs
@@ -10,7 +10,7 @@ namespace RefRestrictTest
     {
         [TestMethod]
         [ExpectedException(typeof(DirectoryNotFoundException))]
-        public void GetRuleSetForAConfigFileWhereDirectoryDoesntExist()
+        public void ThrowExceptionWhenGettingRuleSetForAConfigFileWhereDirectoryDoesntExist()
         {
             // Arrange
             var badConfigFile = @"C:\IDoNotExist\InvalidConfig.xml";
@@ -22,7 +22,7 @@ namespace RefRestrictTest
 
         [TestMethod]
         [ExpectedException(typeof(FileNotFoundException))]
-        public void GetRuleSetForAConfigFileWhereFileDoesntExist()
+        public void ThrowExceptionWhenGettingRuleSetForAConfigFileWhereFileDoesntExist()
         {
             // Arrange
             var badConfigFile = @"C:\InvalidConfig.xml";
@@ -30,6 +30,57 @@ namespace RefRestrictTest
 
             // Act
             ConfigParser.GetRuleSetForProject(badConfigFile, project);
+        }
+
+        [TestMethod]
+        public void GetRuleSetWithNoRulesFromABlankConfigFile()
+        {
+            // Arrange
+            var badConfigFile = @"TestConfigFiles\TestConfig_NoProjects.xml";
+            var project = "RefRestrict";
+
+            // Act
+            var ruleset = ConfigParser.GetRuleSetForProject(badConfigFile, project);
+
+            // Assert
+            Assert.IsNotNull(ruleset);
+            Assert.IsFalse(ruleset.IsRules);
+        }
+
+        [TestMethod]
+        public void GetRuleSetWithNoRulesForAProjectWhichHasNoRules()
+        {
+            // Arrange
+            var badConfigFile = @"TestConfigFiles\TestConfig_ProjectAlphaSingleRule.xml";
+            var project = "ProjectBeta";
+
+            // Act
+            var ruleset = ConfigParser.GetRuleSetForProject(badConfigFile, project);
+
+            // Assert
+            Assert.IsNotNull(ruleset);
+            Assert.IsFalse(ruleset.IsRules);
+        }
+
+        [TestMethod]
+        public void GetRuleSetWithSingleRule()
+        {
+            // Arrange
+            var badConfigFile = @"TestConfigFiles\TestConfig_ProjectAlphaSingleRule.xml";
+            var project = "ProjectAlpha";
+
+            // Act
+            var ruleset = ConfigParser.GetRuleSetForProject(badConfigFile, project);
+
+            // Assert
+            Assert.IsNotNull(ruleset);
+            Assert.IsTrue(ruleset.IsRules);
+            Assert.AreEqual(1, ruleset.Rules.Count);
+            
+            var actRule = ruleset.Rules[0] as MultiRefRule;
+            Assert.IsNotNull(actRule);
+            Assert.AreEqual(RuleType.OnlyLocalReferences, actRule.Type);
+            Assert.AreEqual(0, actRule.Refs.Count);
         }
     }
 }
