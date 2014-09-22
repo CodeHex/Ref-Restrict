b3d06cb5125fd200cfeb5a41c5bf75b4fd5d86eb
Ben Ibinson <codehex.net@gmail.com>
2014-09-22 22:22:05 +0100

    Cleaned up file

--- nuget/tools/install.ps1
+++ nuget/tools/install.ps1
@@ -1,39 +1,44 @@
 param($installPath, $toolsPath, $package, $project)
 
+# Define the Ref Restrict specific files
+$configFileName = "RefRestrict.config.xml"
+$refRestrictExe = "RefRestrict.exe"
+
 # Get the open solution.
 $solution = Get-Interface $dte.Solution ([EnvDTE80.Solution2])
+$solPath = Split-Path -parent $dte.Solution.FileName
+$configPath = $solPath + "/" + $configFileName
+$exePath = $solPath + "/" + $refRestrictExe
 
-# Create Solution Items folder
+# Create Solution Items folder if it doesn't exist
 $solutionFolder = $solution.Projects | where-object { $_.ProjectName -eq "Solution Items" } | select -first 1
-
 if(!$solutionFolder) {
-	$solutionFolder = $solution.AddSolutionFolder("Solution Items")
+    $solutionFolder = $solution.AddSolutionFolder("Solution Items")
 }
 
 # Copy the exe solution folder (overwrite any existing exe)
-$refRestrictExe = join-path $installPath 'tools/RefRestrict.exe'
-Copy-Item $refRestrictExe $rootdir
+$exeToolPath = join-path $installPath ('tools/' + $refRestrictExe)
+Copy-Item $exeToolPath $rootdir
 
 # Copy the blank xml file if it doesn't exist
-$solPath = Split-Path -parent $dte.Solution.FileName
-$configfile = $solPath + "\RefRestrict.config.xml"
+$configPath = $solPath + "/" + $configFileName
 
-if(!(Test-Path $configfile)) {
+if(!(Test-Path $configPath)) {
     # Copy blank config file
-    $refRestrictXml = join-path $installPath 'tools/RefRestrict.config.xml'
-    Copy-Item $refRestrictXml $rootdir
-
+    $configToolPath = join-path $installPath ('tools/' + $configFileName)
+    Copy-Item $configToolPath $rootdir
+    
     # Add a file to the Solution Items folder.
     $solItems = Get-Interface $solutionFolder.ProjectItems ([EnvDTE.ProjectItems])
-    $solItems.AddFromFile($configfile) > $null
+    $solItems.AddFromFile($configPath) > $null
 }
 
 # Add section for project
 $configXml = New-Object XML
-$configXml.Load($configfile)
+$configXml.Load($configPath)
 $ruleNode = $configXml.CreateElement("rules")
 $ruleNode.SetAttribute("project", $project.ProjectName)
 $ruleData = $configXml.CreateComment("Add rules for project " + $project.ProjectName + " here")
 $ruleNode.AppendChild($ruleData) > $null
 $configXml.DocumentElement.AppendChild($ruleNode) > $null
-$configXml.Save($configfile)
+$configXml.Save($configPath)
